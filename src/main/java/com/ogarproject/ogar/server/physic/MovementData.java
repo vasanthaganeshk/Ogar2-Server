db8eb94d9678757693afd3e65299426a79a11683
Romarjosh Villamor <thundergemios10@gmail.com>
2016-04-17 07:04:04 -0700

    Update MovementData.java

--- src/main/java/com/ogarproject/ogar/server/physic/MovementData.java
+++ src/main/java/com/ogarproject/ogar/server/physic/MovementData.java
@@ -1,37 +1,77 @@
 package com.ogarproject.ogar.server.physic;
 
+import com.ogarproject.ogar.api.entity.Entity;
+
 /**
- * Created by Porama2 on 2/4/2016.
+ * Created by Porama2 on 1/4/2016.
  */
-public class MovementData {
-    double angle = 0;
-    double speed = 0;
+public class PhysicsData {
+    final Entity entity;
+    public MovementData movement = new MovementData();
+    double resistance = 0;
 
-    public MovementData(){}
-
-    public MovementData(double angle, double speed){
-        setAngle(angle);
-        setSpeed(speed);
+    public PhysicsData(Entity entityowner) {
+        entity = entityowner;
     }
 
-    public void setSpeed(double speed) {
-        this.speed = speed;
+    public double getResistance() {
+        return resistance;
+    }
+
+    public void setResistance(double resistance) {
+        this.resistance = resistance;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public boolean isUpdateNeeded() {
+        return !(movement.getSpeed() == 0);
+    }
+
+    public Vector getVector() {
+        return Calc.getVectorFromMovement(movement.getAngle(), movement.getSpeed());
+    }
+
+    public void setVector(Vector vector) {
+        movement = Calc.getMovementFromVector(vector);
+    }
+
+    public void setMovement(MovementData record) {
+        movement = record;
     }
 
     public double getSpeed() {
-        return speed;
+        return movement.getSpeed();
     }
 
-    public void setAngle(double angle) {
-        this.angle = angle;
+    public void setSpeed(double speed) {
+        movement.setSpeed(speed);
     }
 
     public double getAngle() {
-        return angle;
+        return movement.getAngle();
     }
 
-    @Override
-    public String toString() {
-        return "Movement(" + angle + "," + speed + ")";
+    public void setAngle(double angle) {
+        movement.setAngle(angle);
+    }
+
+    public void UpdatePosition() {
+        Vector vector = getVector();
+        entity.setPosition(Calc.KeepInWorld(entity.getPosition().add(vector.getX(), 0 - vector.getY())));
+    }
+
+    public void UpdateData() {
+        double speed = movement.getSpeed();
+        speed -= resistance;
+        if (speed < 0) speed = 0;
+        movement.setSpeed(speed);
+    }
+
+    public void Update() {
+        UpdatePosition();
+        UpdateData();
     }
 }
